74f0ce9bef2d7ca4359f0f6eaeb9455ecc4b4589
diqi <ghjapple@163.com>
2026-03-31 10:12:40 +0800

    feat(user-pref): add getPreferencesByPrefix batch query

--- platform/src/main/java/com/auraboot/framework/user/service/UserPreferenceService.java
+++ platform/src/main/java/com/auraboot/framework/user/service/UserPreferenceService.java
@@ -2,9 +2,13 @@ package com.auraboot.framework.user.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import java.util.Map;
+
 public interface UserPreferenceService {
 
     JsonNode getPreference(Long userId, String key);
 
     void setPreference(Long userId, String key, JsonNode value);
+
+    Map<String, JsonNode> getPreferencesByPrefix(Long userId, String prefix);
 }
